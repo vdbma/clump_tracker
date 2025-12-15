cf1778eacefdad67fea4ce57a40a0e1010bd0e4c
marc <vandenbossche.marc@hotmail.com>
2025-12-15 17:21:33 +0100

    Add ref adj list impl

--- tests/test_cc.py
+++ tests/test_cc.py
@@ -2,10 +2,10 @@ import numpy as np
 import pytest
 from numpy.testing import assert_array_equal
 
-from clump_tracker import compute_adjacency_cartesian, compute_cc
+from clump_tracker import compute_adjacency_matrix_cartesian, compute_cc
 
 
-def _compute_adjacency_cartesian_ref(indexes, x, y, z, max_distance):
+def _compute_adjacency_matrix_cartesian_ref(indexes, x, y, z, max_distance):
     """
     computes and returns adjacency matrix
     this is a reference implementation for
@@ -24,6 +24,28 @@ def _compute_adjacency_cartesian_ref(indexes, x, y, z, max_distance):
     return adj
 
 
+def _compute_adjacency_list_cartesian_ref(indexes, x, y, z, max_distance):
+    """
+    computes and returns adjacency list
+    this is a reference implementation for
+    testing purposes only
+    """
+    adj = []
+
+    for i in range(len(indexes)):
+        adj.append([])
+        for j in range(len(indexes)):
+            d2 = (
+                (x[indexes[i][0]] - x[indexes[j][0]]) ** 2
+                + (y[indexes[i][1]] - y[indexes[j][1]]) ** 2
+                + (z[indexes[i][2]] - z[indexes[j][2]]) ** 2
+            )
+            if d2 <= max_distance * max_distance:
+                adj[i].append(j)
+
+    return adj
+
+
 def _compute_cc_ref(indexes, x, y, z, max_distance):
     """
     indexes : list of coordinates (array indexes)
@@ -39,7 +61,7 @@ def _compute_cc_ref(indexes, x, y, z, max_distance):
     a_visiter = np.zeros((len(indexes)), dtype=bool)
     composante_connexes = []
 
-    adj = _compute_adjacency_cartesian_ref(indexes, x, y, z, max_distance)
+    adj = _compute_adjacency_matrix_cartesian_ref(indexes, x, y, z, max_distance)
 
     # ajoute les voisins de p0 dans a_visiter
     for i, _ in enumerate(indexes):
@@ -55,6 +77,49 @@ def _compute_cc_ref(indexes, x, y, z, max_distance):
                 if a_visiter[j] and not deja_vus[j]:
                     composante_connexes[-1].append(j)
                     a_visiter += adj[j]
+                    a_visiter[j] = False
+                    deja_vus[j] = True
+                    a_visiter = np.logical_and(a_visiter, np.logical_not(deja_vus))
+
+        if np.sum(deja_vus) == len(indexes):
+            break
+
+    return composante_connexes
+
+
+def _compute_cc_from_list_ref(indexes, x, y, z, max_distance):
+    """
+    indexes : list of coordinates (array indexes)
+
+    this function checks the distance between all pairs in indexes,
+    if it is less than `max_distance` they are of the same clump
+
+    this is a reference implementation for
+    testing purposes only
+    """
+
+    deja_vus = np.zeros((len(indexes)), dtype=bool)
+    a_visiter = np.zeros((len(indexes)), dtype=bool)
+    composante_connexes = []
+
+    adj = _compute_adjacency_list_cartesian_ref(indexes, x, y, z, max_distance)
+
+    # ajoute les voisins de p0 dans a_visiter
+    for i, _ in enumerate(indexes):
+        if not deja_vus[i]:
+            composante_connexes.append([i])
+        a_visiter = np.array([k in adj[i] for k in range(len(indexes))], dtype=bool)
+        a_visiter[i] = False
+        deja_vus[i] = True
+        a_visiter = np.logical_and(a_visiter, np.logical_not(deja_vus))
+
+        while np.sum(a_visiter) > 0:  # there are still people to visit
+            for j, _ in enumerate(indexes):
+                if a_visiter[j] and not deja_vus[j]:
+                    composante_connexes[-1].append(j)
+                    a_visiter += np.array(
+                        [k in adj[i] for k in range(len(indexes))], dtype=bool
+                    )
                     a_visiter[j] = False
                     deja_vus[j] = True
                     a_visiter = np.logical_and(a_visiter, np.logical_not(deja_vus))
@@ -81,14 +146,36 @@ def dtype(request):
     return request.param
 
 
-def test_adjacency(indexes, dtype):
+def test_adjacency_list_ref(indexes):
+    x = np.linspace(0, 50, 51)
+    y = np.linspace(0, 50, 51)
+    z = np.linspace(0, 50, 51)
+    print(x, y, z)
+
+    idx = [
+        [[0, 0, 0], [1, 0, 0], [2, 0, 1]],
+        [[0, 0, 0], [49, 19, 4]],
+        [[i, 0, 0] for i in range(50)],
+    ]
+
+    lst_1 = [
+        [[0, 1], [0, 1], [2]],
+        [[0], [1]],
+        [[0, 1]] + [[i - 1, i, i + 1] for i in range(1, 49)] + [[48, 49]],
+    ]
+
+    for i, L in zip(idx, lst_1, strict=False):
+        assert _compute_adjacency_list_cartesian_ref(i, x, y, z, 1.0) == L
+
+
+def test_adjacency_matrix(indexes, dtype):
     x = np.linspace(0, 10, 50, dtype=dtype)
     y = np.linspace(0, 5, 20, dtype=dtype)
     z = np.linspace(0, 1, 5, dtype=dtype)
 
     assert_array_equal(
-        compute_adjacency_cartesian(indexes, x, y, z, 1.0),
-        _compute_adjacency_cartesian_ref(indexes, x, y, z, 1.0),
+        compute_adjacency_matrix_cartesian(indexes, x, y, z, 1.0),
+        _compute_adjacency_matrix_cartesian_ref(indexes, x, y, z, 1.0),
     )
 
 
@@ -129,6 +216,17 @@ def test_cc_ref(cc_params):
     d, expected = cc_params
 
     actual = _compute_cc_ref(indexes, x, y, z, d)
+    assert actual == expected
+
+
+def test_cc_list_ref(cc_params):
+    indexes = [[i, 0, 0] for i in range(3)]
+    x = np.array([0, 1, 10])
+    y = np.array([0])
+    z = np.array([0])
+    d, expected = cc_params
+
+    actual = _compute_cc_from_list_ref(indexes, x, y, z, d)
     assert actual == expected
 
 
