452aac3f3edd1e6f4d88e1c50609dc746c6129d0
marc <vandenbossche.marc@hotmail.com>
2025-12-20 20:40:31 +0100

    Better tests

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -7,15 +7,16 @@ from numpy.testing import assert_array_almost_equal, assert_array_equal
 from clump_tracker import gradient
 
 
-def test_tests():
-    assert True
+@pytest.fixture(params=[np.float32, np.float64, float])
+def dtype(request):
+    return request.param
 
 
-def _dtype():
-    return "dtype", [float, np.float32, np.float64]
+@pytest.fixture(params=[0, 1, 2])
+def axis(request):
+    return request.param
 
 
-@pytest.mark.parametrize(*_dtype())
 def test_gradient_1D_uniform(dtype):
     field = np.arange(10, dtype=dtype) + 5
     x = np.arange(10, dtype=dtype)
@@ -23,7 +24,6 @@ def test_gradient_1D_uniform(dtype):
     assert_array_equal(gradient(field, x, axis), np.gradient(field, x, axis=axis))
 
 
-@pytest.mark.parametrize(*_dtype())
 def test_gradient_1D_non_uniform(dtype):
     field = np.arange(10, dtype=float) + 5
     axis = 0
@@ -33,18 +33,12 @@ def test_gradient_1D_non_uniform(dtype):
     )
 
 
-def _dtype_and_axis():
-    return "axis,dtype", product(range(3), [float, np.float32, np.float64])
-
-
-@pytest.mark.parametrize(*_dtype_and_axis())
 def test_gradient_3D_uniform(axis, dtype):
     field = np.arange(9 * 10 * 11, dtype=dtype).reshape((9, 10, 11)) + 5
     x = np.arange(field.shape[axis], dtype=dtype)
     assert_array_equal(gradient(field, x, axis), np.gradient(field, x, axis=axis))
 
 
-@pytest.mark.parametrize(*_dtype_and_axis())
 def test_gradient_3D_non_uniform(axis, dtype):
     field = np.arange(9 * 10 * 11, dtype=dtype).reshape((9, 10, 11)) + 5
     x = np.geomspace(1, 10, field.shape[axis], dtype=dtype)
